de71d56a270bf5286dd2a999aa7bdae0724b670c
Chris Maguire <cwmaguire@gmail.com>
2015-11-04 16:03:22 -0800

    Solve for every possible solution
    Given a specification for overlapping sets of numbered tiles that must
    contain a certain number of mines calculate all possible solutions
    containing the numbered cells containing mines.
    
    An example spec would be:
    [{[1,2,3,15,16],2},
     {[2,3,4],1},
     {[3,4,5,6,7],2},
     {[6,7,8],1},
     {[7,8,9,10,11],2},
     {[10,11,12],1},
     {[11,12,13,14,15],1},
     {[14,15,16],1}]
    
    Cells 1-3, 15 and 16 must have 2 mines. That is, some cell connected to
    all of those cells has reported that it is adjacent to two mines.
    
    The example spec would be for a section of a "mines" / "minesweeper"
    like the following:
    
    1   2  3  4  5  The numbers on the outside are the cells that mines
    16  2  1  2  6  can be in and the numbers on the inside are the
    15  1     1  7  reported numbers of adjacent mines.
    14  1  1  2  8
    13  12 11 10 9

--- mines.erl
+++ mines.erl
@@ -3,12 +3,14 @@
 -export([solutions/1]).
 
 -record(step,
-        {picked :: [integer()],
-         maxed :: [integer()],
-         groups :: [{[integer()], integer()}]}).
+        {picked = [] :: [integer()],
+         maxed = [] :: [integer()],
+         groups = [] :: [{[integer()], integer()}]}).
 
 solutions(Groups) when is_list(Groups) ->
-    lists:usort(lists:flatten(solutions(#step{groups = Groups})));
+    Solutions = lists:usort(lists:flatten(solutions(#step{groups = Groups}))),
+    ValidSolutions = lists:filter(fun(Tuple) -> element(1, Tuple) /= bad_solution end, Solutions),
+    lists:sort([lists:sort(tuple_to_list(S)) || S <- ValidSolutions]);
 solutions(#step{picked = Picked, groups = []}) ->
     list_to_tuple(lists:flatten(Picked));
 solutions(Step = #step{picked = Picked,
@@ -16,28 +18,39 @@ solutions(Step = #step{picked = Picked,
                        groups = [{Spots, 0} | Groups]}) ->
     solutions(Step#step{maxed = lists:usort(Picked ++ MaxedOut ++ Spots),
                         groups = Groups});
-solutions(Step = #step{groups = [{Spots, _} | _]}) ->
-    case lists:foldl(fun valid_solutions/2, {Step, []}, Spots) of
-        {_, []} ->
+solutions(Step = #step{picked = Picked,
+                       maxed = Maxed,
+                       groups = [{Spots, Max} | Groups]}) ->
+    case num_picked(Picked, Spots) of
+        X when X == Max ->
+            solutions(Step#step{groups = Groups, maxed = lists:usort(Maxed ++ Spots)});
+        X when X > Max ->
             list_to_tuple(lists:flatten([bad_solution | Step#step.picked]));
-        {_, NewSteps} ->
-            [solutions(NewStep) || NewStep <- NewSteps]
+        _ ->
+            case lists:foldl(fun valid_solutions/2, {Step, []}, Spots) of
+                {_, []} ->
+                    list_to_tuple(lists:flatten([bad_solution | Step#step.picked]));
+                {_, NewSteps} ->
+                    [solutions(NewStep) || NewStep <- NewSteps]
+            end
     end.
 
 valid_solutions(Spot, {Step = #step{groups = [{Spots, Max} | Groups]}, Steps}) ->
     case is_valid(Spot, Step) of
-        false ->
+        {_, true} ->
             {Step, Steps};
-        true ->
+        {true, false} ->
+            NewStep = Step#step{groups = [{Spots -- [Spot], Max - 1} | Groups]},
+            {Step, [NewStep | Steps]};
+        {false, false} ->
             NewStep = Step#step{picked = [Spot | Step#step.picked],
                                 groups = [{Spots -- [Spot], Max - 1} | Groups]},
             {Step, [NewStep | Steps]}
     end.
 
-%is_valid(_, Spots, _Picked, MaxedOut, _Max = 1) ->
-    %is_disjoint(Spots, MaxedOut);
 is_valid(Spot, #step{picked = Picked, maxed = Maxed}) ->
-    not lists:member(Spot, Picked ++ Maxed).
+    {lists:member(Spot, Picked),
+     lists:member(Spot, Maxed)}.
 
-%is_disjoint(A, B) ->
-    %A -- B == A.
+num_picked(Picked, Spots) ->
+    length([S || S <- Spots, lists:member(S, Picked)]).
